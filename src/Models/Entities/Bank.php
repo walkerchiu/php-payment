fb18e26d33f469d50cbfe01094c3043c83fc5af0
walkerchiu <chenjen.chiou@gmail.com>
2022-08-11 22:14:05 +0800

    Initial commit

---
+++ src/Models/Entities/Bank.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace WalkerChiu\Payment\Models\Entities;
+
+use WalkerChiu\Core\Models\Entities\LangTrait;
+use WalkerChiu\Core\Models\Entities\UuidEntity;
+
+class Bank extends UuidEntity
+{
+    use LangTrait;
+
+
+
+    /**
+     * Create a new instance.
+     *
+     * @param Array  $attributes
+     * @return void
+     */
+    public function __construct(array $attributes = [])
+    {
+        $this->table = config('wk-core.table.payment.banks');
+
+        $this->fillable = array_merge($this->fillable, [
+            'host_id',
+            'swift_id', 'bank_id', 'branch_id', 'account_number', 'account_name'
+        ]);
+
+        parent::__construct($attributes);
+    }
+
+    /**
+     * @return \Illuminate\Database\Eloquent\Relations\BelongsTo
+     */
+    public function host()
+    {
+        return $this->belongsTo(config('wk-core.class.payment.payment'), 'host_id', 'id');
+    }
+}
